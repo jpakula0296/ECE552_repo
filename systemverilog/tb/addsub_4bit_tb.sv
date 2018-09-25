04b1931e6c79fd3f04ae71e5e64d292a844bae12
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-09-25 11:39:28 -0500

    added subtraction functionality to first draft

--- systemverilog/tb/addsub_4bit_tb.sv
+++ systemverilog/tb/addsub_4bit_tb.sv
@@ -1,1 +1,11 @@
 module addsub_4bit_tb();
+
+// inputs are registers
+reg [3:0] A, B; // inputs to operation
+reg sub; // subtraction if high addition if overflow
+
+// outputs are wires
+wire [3:0] Sum;
+wire Ovfl;
+
+// Instantiate DUT
