f77f4a4141c96a84f70ec6fbea86fb332e453ba3
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-09-25 10:54:20 -0500

    finished full adder

--- systemverilog/full_adder.sv
+++ systemverilog/full_adder.sv
@@ -3,6 +3,22 @@ module full_adder(
   input B,
   input Cin,
 
-  output Sum,
-  output Cout;
+  output sum,
+  output Cout
   );
+
+// declare and assign internal wires
+wire xor_out;
+wire and0_out;
+wire and1_out;
+
+assign xor_out = A ^ B;
+assign and0_out = A & B;
+assign and1_out = xor_out & Cin;
+
+// assign outputs
+assign sum = xor_out ^ Cin;
+assign Cout = and0_out | and1_out;
+
+
+endmodule
