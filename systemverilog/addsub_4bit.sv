31eb8788908091a3ac2e5fa991226f50b552dc06
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-09-25 12:23:54 -0500

    fixed first Cin to be 1b0

--- systemverilog/addsub_4bit.sv
+++ systemverilog/addsub_4bit.sv
@@ -15,10 +15,11 @@ assign B_not = ~B;
 wire Bout0;
 wire Bout1;
 wire Bout2;
+wire Bout3;
 full_adder_1bit FA4 (.A(1'b1), .B(B[0]), .Cin(1'b0), .S(B_negated[0]), .Cout(Bout0));
 full_adder_1bit FA5 (.A(1'b0), .B(B[1]), .Cin(Bout0), .S(B_negated[1]), .Cout(Bout1));
 full_adder_1bit FA6 (.A(1'b0), .B(B[2]), .Cin(Bout1), .S(B_negated[2]), .Cout(Bout2));
-full_adder_1bit FA7 (.A(1'b0), .B(B[3]), .Cin(Bout2), .S(B_negated[3]), .Cout(1'bx));
+full_adder_1bit FA7 (.A(1'b0), .B(B[3]), .Cin(Bout2), .S(B_negated[3]), .Cout(Bout3));
 
 // mux B input depending on sub control signal
 wire [3:0] B_input;
@@ -31,7 +32,7 @@ wire Cout2;
 wire Cout3;
 
 // Connect inputs and sum output, propogate carry ins and carry outs
-full_adder_1bit FA0 (.A(A[0]), .B(B_input[0]), .Cin(Cin), .S(Sum[0]), .Cout(Cout0));
+full_adder_1bit FA0 (.A(A[0]), .B(B_input[0]), .Cin(1'b0), .S(Sum[0]), .Cout(Cout0));
 full_adder_1bit FA1 (.A(A[1]), .B(B_input[1]), .Cin(Cout0), .S(Sum[1]), .Cout(Cout1));
 full_adder_1bit FA2 (.A(A[2]), .B(B_input[2]), .Cin(Cout1), .S(Sum[2]), .Cout(Cout2));
 full_adder_1bit FA3 (.A(A[3]), .B(B_input[3]), .Cin(Cout2), .S(Sum[3]), .Cout(Cout3));
