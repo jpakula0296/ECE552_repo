118c5090a6d6790ba572a3c02036d33c9a80870d
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-10-14 16:39:21 -0500

    continue work on data_mem_tb

--- systemverilog/data_mem_control.v
+++ systemverilog/data_mem_control.v
@@ -12,12 +12,16 @@ module data_mem_control(
 wire [15:0] offset_16;
 wire [15:0] offset_shift;
 wire [15:0] offset_ext; // immediate shifted left 1 and sign extended
+wire [15:0] offset_plus_rs;
 
 assign offset_16 = {12'b0, offset};
 assign offset_shift = offset_16 << 1;
 assign offset_ext = {{11{offset_shift[4]}}, offset_shift[4:0]};
 
 // add rs and processed immedate to get target address
-rca_16bit add0(.a(rsData), .b(offset_ext), .cin(1'b0), .s(target_addr), .cout());
+rca_16bit add0(.a(rsData), .b(offset_ext), .cin(1'b0), .s(offset_plus_rs), .cout());
+
+assign target_addr = offset_plus_rs & 16'hFFFE; // last bit 0 (word boudary)
+
 
 endmodule
--- systemverilog/tb/data_mem_control_tb.sv
+++ systemverilog/tb/data_mem_control_tb.sv
@@ -9,20 +9,34 @@ wire[15:0] data_out;
 data_mem_control iDUT(.rsData(rsData), .rtData(rtData), .offset(offset),
   .target_addr(target_addr));
 
-memory1c data_mem(.data_out(data_out), .data_in(rtData), .addr(target_addr),
+data_mem data_memory(.data_out(data_out), .data_in(rtData), .addr(target_addr),
   .enable(1'b1), .wr(wr), .clk(clk), .rst(rst));
 
 initial begin
   rsData = 0;
-  rtData = 0;
+  rtData = 16'hBEEF;
   offset = 0;
   clk = 0;
   rst = 1;
   wr = 0;
+  // try a write operation at address 0 first
+  repeat(2) @(posedge clk);
 
+  wr = 1;
+  rst = 0;
+  @(posedge clk);
 
+  wr = 0;
+  rtData = 16'hDEAD;
+  rsData = 0;
+  offset = 1;
+  @(posedge clk);
+  wr = 1;
+  @(posedge clk);
+  wr = 0;
+  @(posedge clk);
 
-
+$stop();
 end
 
 always begin
