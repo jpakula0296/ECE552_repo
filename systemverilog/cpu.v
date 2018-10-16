67ad8083b09f260abe60fb1f5615a8cb8fb54591
Vinay Janardhanam <vjanard@gmail.com>
2018-10-15 22:27:22 -0500

    Fixed name for Z_in

--- systemverilog/cpu.v
+++ systemverilog/cpu.v
@@ -87,7 +87,7 @@ assign DstData =
 
 RegisterFile regfile(.clk(clk), .rst(rst), .WriteReg(WriteReg), .SrcReg1(rs),
   .SrcReg2(rt), .DstReg(rd), .SrcData1(rsData), .SrcData2(rtData),
-  .DstData(DstData), .Z_in(Zin), .V_in(V_in), .N_in(N_in), .Z_out(Z_out),
+  .DstData(DstData), .Z_in(Z_in), .V_in(V_in), .N_in(N_in), .Z_out(Z_out),
   .N_out(N_out), .V_out(V_out), .load_half_instr(load_half_instr));
 
 // ALU
