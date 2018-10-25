94b06cb68f3f134cb74b12babc34c55af30d76d5
Vinay Janardhanam <vjanard@gmail.com>
2018-10-25 14:15:08 -0500

    changed a couple more names

--- systemverilog/WB_data.v
+++ systemverilog/WB_data.v
@@ -3,15 +3,15 @@ module WB_data(
   input flush,
   input stall_n,
   input WriteReg_in,
-  input [15:0] ALU_result_in,
+  input [15:0] ALU_res_in,
   input [15:0] data_mem_in,
   output WriteReg_out,
-  output [15:0] ALU_result_out,
+  output [15:0] ALU_res_out,
   output [15:0] data_mem_out
   );
 
 dff write_reg_dff(.d(WriteReg_in), .q(WriteReg_out), .wen(wen), .clk(clk), .rst(rst));
-dff_16bit ALU_res_dff(.d(ALU_result_in), .q(ALU_result_out), .wen(wen), .clk(clk), .rst(rst));
+dff_16bit ALU_res_dff(.d(ALU_res_in), .q(ALU_res_out), .wen(wen), .clk(clk), .rst(rst));
 dff_16bit data_mem_dff(.d(data_mem_in), .q(data_mem_out), .wen(wen), .clk(clk), .rst(rst));
 
 
