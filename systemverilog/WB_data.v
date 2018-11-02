57379c6365f0c26aafc631a1a5f87fe1328cab04
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-11-01 20:04:31 -0500

    fix wb_data for real this time

--- systemverilog/WB_data.v
+++ systemverilog/WB_data.v
@@ -10,9 +10,9 @@ module WB_data(
   output [15:0] data_mem_out
   );
 
-dff write_reg_dff(.d(WriteReg_in), .q(WriteReg_out), .wen(stall_n), .clk(clk), .rst(stall_n));
-dff_16bit ALU_res_dff(.d(ALU_res_in), .q(ALU_res_out), .wen(stall_n), .clk(clk), .rst(stall_n));
-dff_16bit data_mem_dff(.d(data_mem_in), .q(data_mem_out), .wen(stall_n), .clk(clk), .rst(stall_n));
+dff write_reg_dff(.d(WriteReg_in), .q(WriteReg_out), .wen(stall_n), .clk(clk), .rst(flush));
+dff_16bit ALU_res_dff(.d(ALU_res_in), .q(ALU_res_out), .wen(stall_n), .clk(clk), .rst(flush));
+dff_16bit data_mem_dff(.d(data_mem_in), .q(data_mem_out), .wen(stall_n), .clk(clk), .rst(flush));
 
 
 endmodule
