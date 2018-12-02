c78e1e821b93d64cb6f5e3d19fb7a31c3d745424
jpakula0296 <38566663+jpakula0296@users.noreply.github.com>
2018-12-01 20:34:14 -0600

    Instantiated DataArray and two MetaDataArrays in cache module

--- systemverilog/memory/cache.v
+++ systemverilog/memory/cache.v
@@ -10,5 +10,30 @@ module cache(data_out, data_in, addr, enable, wr, clk, rst, arbiter_select);
    input          rst;
    input          arbiter_select;
 
+// set BlockEnable and WordEnable based on address
+wire [127:0] DataBlockEnable, MetaBlockEnable0, MetaBlockEnable1;
+wire [7:0] WordEnable;
+
+wire [7:0] MetaData_in;
+wire [7:0] MetaData0_out, MetaData1_out;
+
+
+
+DataArray dataArray(.clk(clk), .rst(rst), .DataIn(data_in), .Write(wr),
+.BlockEnable(DataBlockEnable), .WordEnable(WordEnable), .DataOut(data_out));
+
+MetaDataArray metaDataArray0(.clk(clk), .rst(rst), .DataIn(MetaData_in),
+.Write(wr), .BlockEnable(MetaBlockEnable0), .DataOut(MetaData0_out));
+
+MetaDataArray metaDataArray1(.clk(clk), .rst(rst), .DataIn(MetaData_in),
+.Write(wr), .BlockEnable(MetaBlockEnable1), .DataOut(MetaData1_out));
+
+
+
+
+
+
+
+
 
 endmodule
